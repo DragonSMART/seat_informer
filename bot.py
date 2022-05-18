9d224cd55f04a6641db5af447c8d6d8a3070107f
DragonSMART <smartdrake@gmail.com>
2022-05-18 18:36:51 +0300

    тиккер можно писать любой раскладкой

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ class DiscordBot(discord.Client):
         need_corp_id = None
         need_corp_name = None
 
-        if self.database.get_corp_ticker(corp_ticker=extra_command[0]):
+        if self.database.get_corp_ticker(corp_ticker=extra_command[0].uppper()):
             temp_corp_info = self.database.get_corp_ticker(corp_ticker=extra_command[0])
             need_corp_id = int(temp_corp_info[0])
             need_corp_name = temp_corp_info[1]
